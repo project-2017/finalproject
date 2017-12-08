0ab2cc24e722e9a690e49f5dd727b2267be9c5f0
cliff <liu00399@algonquinlive.com>
2017-12-07 19:49:28 -0500

    Geyan fix small bugs

--- app/src/main/java/cst2335/groupproject/PkgHouse/H_Main.java
+++ app/src/main/java/cst2335/groupproject/PkgHouse/H_Main.java
@@ -1,9 +1,5 @@
 package cst2335.groupproject.PkgHouse;
 
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -11,40 +7,55 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import cst2335.groupproject.PkgHouse.homeThermo_mainPkg.HouseThermostatActivity;
+import cst2335.groupproject.PkgMain.M_SharedPreference;
 import cst2335.groupproject.R;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * This class is used for creating the main GUI of house thermostat
  */
 public class H_Main extends Fragment {
 
+    /**
+     * Using M_SharedPreference
+     */
+    private M_SharedPreference sharedPreference = new M_SharedPreference();
 
+    /**
+     * The construction
+     */
     public H_Main() {
         // Required empty public constructor
     }
 
-
+    /**
+     * On create view
+     *
+     * @param inflater           The inflater
+     * @param container          The container
+     * @param savedInstanceState The savedInstanceState
+     * @return The view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.house_main, container, false);
 
-        Intent intent = new Intent(view.getContext(), HouseThermostatActivity.class);
-        startActivity(intent);
+        // Set current layout
+        sharedPreference.setLayout(view.getContext(), "H_Main");
 
-        SharedPreferences sharedPreferences = view.getContext().getSharedPreferences("Layout", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("Name", "H_Main");
-        editor.apply();
         return view;
     }
 
+    /**
+     * On resume
+     */
     @Override
     public void onResume() {
         super.onResume();
+
+        // Set title for action bar
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.main_menu_house);
     }
 }
--- app/src/main/java/cst2335/groupproject/PkgMain/M_MainPage.java
+++ app/src/main/java/cst2335/groupproject/PkgMain/M_MainPage.java
@@ -1,6 +1,5 @@
 package cst2335.groupproject.PkgMain;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import cst2335.groupproject.PkgActivity.T_Help;
 import cst2335.groupproject.PkgActivity.T_Main;
 import cst2335.groupproject.PkgAutomobile.A_Main;
 import cst2335.groupproject.PkgFood.F_Main;
@@ -87,27 +85,9 @@ public class M_MainPage extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         this.menu = menu;
-        getMenuInflater().inflate(R.menu.tracker_menu, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
 
-        // Only show menu when using activity tracker
-        String name = sharedPreference.getLayout(this);
-        if (name.equals("T_Fragment_ActivityList") || name.equals("T_Fragment_Dashboard")) {
-            showTrackerHelp(true);
-        } else {
-            showTrackerHelp(false);
-        }
         return true;
-    }
-
-    /**
-     * Function for hiding and showing menu
-     *
-     * @param showMenu The boolean that decide show or hide menu
-     */
-    public void showTrackerHelp(boolean showMenu) {
-        if (menu == null)
-            return;
-        menu.setGroupVisible(R.id.tracker_menu_group, showMenu);
     }
 
     /**
@@ -125,14 +105,12 @@ public class M_MainPage extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         // Menu item help
-        if (id == R.id.tracker_menu_action_help) {
-            Intent intent = new Intent(M_MainPage.this, T_Help.class);
-            startActivity(intent);
+        if (id == R.id.main_action_help) {
             return true;
         }
 
         // Menu item quit
-        if (id == R.id.tracker_menu_action_quit) {
+        if (id == R.id.main_action_quit) {
             finishAffinity();
             return true;
         }
@@ -150,12 +128,10 @@ public class M_MainPage extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        showTrackerHelp(false);
 
         int id = item.getItemId();
 
         if (id == R.id.main_drawer_nav_activity) {
-            showTrackerHelp(true);
             T_Main fragment = new T_Main();
             android.support.v4.app.FragmentTransaction fragmentTransaction =
                     getSupportFragmentManager().beginTransaction();
