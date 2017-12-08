b427897ddf7ad59bf9c9ababcd04cc5461a302ca
EXIT2652 <shen0082@algonquinlive.com>
2017-12-08 00:04:42 -0500

    First commit: copied all files from individual to group.

---
+++ app/src/main/java/cst2335/groupproject/PkgFood/FoodAdd.java
@@ -0,0 +1,147 @@
+package cst2335.groupproject.PkgFood;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import cst2335.groupproject.R;
+
+public class FoodAdd extends Activity {
+
+    /*    protected static final String ACTIVITY_NAME = "AddItemsActivity";
+        private String foodAdd = FoodAddActivity.class.getSimpleName();*/
+    private EditText addFoodName, addServings, addCalories, addFat, addCarbohydrate, addDate, addTime;
+    private String addFoodAndServings;
+    private Button buttonAdd;
+    private FoodDatabaseHelper foodDatabaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_add);
+
+        addFoodName = (EditText) findViewById(R.id.addFoodName);
+        addServings = (EditText) findViewById(R.id.addServings);
+        addCalories = (EditText) findViewById(R.id.addCalories);
+        addFat = (EditText) findViewById(R.id.addFat);
+        addCarbohydrate = (EditText) findViewById(R.id.addCarbo);
+        addDate = (EditText) findViewById(R.id.addDate);
+        addTime = (EditText) findViewById(R.id.addTime);
+        buttonAdd = (Button) findViewById(R.id.addButton);
+        foodDatabaseHelper = new FoodDatabaseHelper(this);
+
+        foodDatabaseHelper.openDatabase();
+
+        //Get a calendar using the default locale and time-zone
+        final Calendar myCalendar = Calendar.getInstance();
+        final Calendar myCurrentTime = Calendar.getInstance();
+
+        //Pop up date picker dialog when user clicks Date edit text
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                String dateFormat = "MM/dd/yyyy";
+                SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.CANADA);
+                addDate.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+
+        addDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(FoodAdd.this, date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        //Pop up time picker dialog when user clicks Time edit text
+        addTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int hour = myCurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = myCurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog myTimePicker;
+                myTimePicker = new TimePickerDialog(FoodAdd.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        addTime.setText(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, false);
+                myTimePicker.show();
+            }
+        });
+
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!addFoodName.getText().toString().equals("") && !addServings.getText().toString().equals("")
+                        && !addCalories.getText().toString().equals("") && !addFat.getText().toString().equals("")
+                        && !addCarbohydrate.getText().toString().equals("") && !addDate.getText().toString().equals("")
+                        && !addTime.getText().toString().equals("")) {
+
+                    if(Integer.parseInt(addServings.getText().toString()) <= 0) {
+                        Toast.makeText(FoodAdd.this, R.string.food_Add_serving_warning, Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        //Insert records into database
+                        boolean isInserted = foodDatabaseHelper.insertData(addFoodName.getText().toString(),
+                                addServings.getText().toString(),
+                                addCalories.getText().toString(),
+                                addFat.getText().toString(),
+                                addCarbohydrate.getText().toString(),
+                                addDate.getText().toString(),
+                                addTime.getText().toString());
+                        if (isInserted == true) {
+                            Toast.makeText(FoodAdd.this, R.string.food_Add_infoAddSuccessful, Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else
+                            Toast.makeText(FoodAdd.this, R.string.food_Add_infoAddUnsuccessful, Toast.LENGTH_SHORT).show();
+                    }
+/*                    Intent resultIntent = new Intent();
+
+                    if (addServings.getText().toString().equals("1")) {
+                        addFoodAndServings = addServings.getText().toString() + " " + addFoodName.getText().toString();
+                    } else
+                        addFoodAndServings = addServings.getText().toString() + " " + addFoodName.getText().toString() + "s";
+
+                    resultIntent.putExtra("addFoodAndServings", addFoodAndServings);
+                    setResult(1, resultIntent);
+                    finish();//Go back to FoodListView class after user presses Add button*/
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.food_Add_empty_warning, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    //Click on cross image to close current activity
+    public void close_return(View view) {
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        foodDatabaseHelper.closeDatabase();
+    }
+}
---
+++ app/src/main/java/cst2335/groupproject/PkgFood/FoodDatabaseHelper.java
@@ -0,0 +1,121 @@
+package cst2335.groupproject.PkgFood;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class FoodDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "FoodInfo.db";
+    private static final int DATABASE_VERSION = 1;
+    private SQLiteDatabase database;
+    private String foodDatabase_className =FoodDatabaseHelper.class.getSimpleName();
+
+    public static final String TABLE_NAME = "foodInfo";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_FOOD_NAME = "foodName";
+    public static final String COLUMN_SERVINGS = "foodServings";
+    public static final String COLUMN_CALORIES = "calories";
+    public static final String COLUMN_FAT = "fat";
+    public static final String COLUMN_CARBOHYDRATE = "carbohydrate";
+    public static final String COLUMN_DATE = "date";
+    public static final String COLUMN_TIME = "time";
+
+
+    public static String[] FOOD_LIST_FIELDS = new String[] {
+            COLUMN_FOOD_NAME,
+            COLUMN_SERVINGS,
+            COLUMN_CALORIES,
+            COLUMN_FAT,
+            COLUMN_CARBOHYDRATE,
+            COLUMN_DATE,
+            COLUMN_TIME
+    };
+
+    private static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+            COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            COLUMN_FOOD_NAME + " TEXT," +
+            COLUMN_SERVINGS + " INTEGER," +
+            COLUMN_CALORIES + " INTEGER," +
+            COLUMN_FAT + " INTEGER," +
+            COLUMN_CARBOHYDRATE + " INTEGER," +
+            COLUMN_DATE + " TEXT," +
+            COLUMN_TIME + " TEXT" +
+            ")";
+
+    public FoodDatabaseHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
+        Log.i(foodDatabase_className, "Calling onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+        Log.i(foodDatabase_className, "Calling onUpgrade, " +
+                "oldVersion =" + oldVersion + "newVersion =" + newVersion);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
+        Log.i(foodDatabase_className, "Calling onDowngrade, " +
+                "oldVersion =" + oldVersion + "newVersion =" + newVersion);
+    }
+
+    public void openDatabase() {
+        database = this.getWritableDatabase();
+    }
+
+    public void closeDatabase() {
+        if(database != null && database.isOpen()){
+            database.close();
+        }
+    }
+
+    public boolean insertData(String foodName, String foodServings, String calories, String fat, String carbohydrate, String date, String time) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_FOOD_NAME, foodName);
+        values.put(COLUMN_SERVINGS, Integer.parseInt(foodServings));
+        values.put(COLUMN_CALORIES, Integer.parseInt(calories));
+        values.put(COLUMN_FAT, Integer.parseInt(fat));
+        values.put(COLUMN_CARBOHYDRATE, Integer.parseInt(carbohydrate));
+        values.put(COLUMN_DATE, date);
+        values.put(COLUMN_TIME, time);
+        long result = database.insert(TABLE_NAME, null, values);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public void updateData(String id, String foodName, String foodServings, String calories, String fat, String carbohydrate, String date, String time) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_FOOD_NAME, foodName);
+        values.put(COLUMN_SERVINGS, Integer.parseInt(foodServings));
+        values.put(COLUMN_CALORIES, Integer.parseInt(calories));
+        values.put(COLUMN_FAT, Integer.parseInt(fat));
+        values.put(COLUMN_CARBOHYDRATE, Integer.parseInt(carbohydrate));
+        values.put(COLUMN_DATE, date);
+        values.put(COLUMN_TIME, time);
+        database.update(TABLE_NAME, values, COLUMN_ID + " = " + id, null);
+    }
+
+    public void deleteData(String id) {
+        database.execSQL("DELETE FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = " + id);
+/*        database.delete(TABLE_NAME, "id = ?", new String[] {id});*/
+    }
+
+    public Cursor getRecords() {
+        return database.query(TABLE_NAME, null, null, null, null, null, null);
+    }
+}
