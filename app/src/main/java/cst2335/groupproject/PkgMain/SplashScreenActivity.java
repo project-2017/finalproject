82753b8ce235b324a93a55eff04b691f5b2a60fa
EXIT2652 <shen0082@algonquinlive.com>
2018-01-02 19:17:01 -0500

    The finalized group project after demonstration.

---
+++ app/src/main/java/cst2335/groupproject/PkgMain/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package cst2335.groupproject.PkgMain;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.Window;
+
+import cst2335.groupproject.R;
+
+public class SplashScreenActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreenActivity.this,M_MainPage.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
